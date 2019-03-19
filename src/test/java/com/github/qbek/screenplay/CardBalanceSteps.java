83e42fa758d3787d79cf82b7046de2c53187fc2a
Olejnik <e-lzok@gft.com>
2019-03-19 12:44:29 +0100

    Code refactor

--- src/test/java/com/github/qbek/screenplay/CardBalanceSteps.java
+++ src/test/java/com/github/qbek/screenplay/CardBalanceSteps.java
@@ -1,10 +1,6 @@
 package com.github.qbek.screenplay;
 
-import abilities.Card;
-import abilities.Credentials;
-import abilities.UseAccount;
-import abilities.UseCards;
-import com.github.javafaker.Faker;
+import abilities.*;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
@@ -13,8 +9,6 @@ import cucumber.api.java.en.When;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.actors.Cast;
 import net.serenitybdd.screenplay.actors.OnStage;
-
-import javax.jws.soap.SOAPBinding;
 
 public class CardBalanceSteps {
 
@@ -25,27 +19,9 @@ public class CardBalanceSteps {
 
     @Given("^(\\w+) is a card user with active account$")
     public void carlIsACardUserWithActiveAccount(String name) throws Throwable {
-        Faker faker = new Faker();
-
         Actor user = OnStage.theActorCalled(name).describedAs("is a card user with active account");
-        Card card = new Card(
-                faker.numerify("11034## ### ###"),
-                faker.business().creditCardExpiry(),
-                faker.number().numberBetween(10, 100)
-        );
-
-        UseCards useCards = new UseCards(card);
-
-        Credentials creds = new Credentials(
-                faker.dragonBall().character(),
-                faker.witcher().monster()
-        );
-
-        UseAccount useAccount = new UseAccount(creds);
-
-        user.can(useCards);
-        user.can(useAccount);
-
+        user.can(Uses.useCreditCard());
+        user.can(Uses.useAccount());
     }
 
     @And("^(\\w+) is logged in his account$")
@@ -54,7 +30,7 @@ public class CardBalanceSteps {
 
         Credentials creds = user.usingAbilityTo(UseAccount.class).getCredentials();
 
-        System.out.println(creds.getLogin()+" "+creds.getPassword());
+        System.out.println(creds.getLogin() + " " + creds.getPassword());
     }
 
     @Then("^correct balance is presented$")
